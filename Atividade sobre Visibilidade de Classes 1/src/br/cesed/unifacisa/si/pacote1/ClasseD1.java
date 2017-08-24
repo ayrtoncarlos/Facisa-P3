3e9ffe9af52ef361f3a3b6876b7c2f807d4741ac
ayrtoncarlos <31282635+ayrtoncarlos@users.noreply.github.com>
2017-08-24 20:20:07 -0300

    Update ClasseD1.java

--- Atividade sobre Visibilidade de Classes 1/src/br/cesed/unifacisa/si/pacote1/ClasseD1.java
+++ Atividade sobre Visibilidade de Classes 1/src/br/cesed/unifacisa/si/pacote1/ClasseD1.java
@@ -5,19 +5,19 @@ import br.cesed.unifacisa.si.pacote2.ClasseP2;
 class ClasseD1 {
 
 	/*
-	 * OBSERVA«’ES:
+	 * OBSERVA√á√ïES:
 	 * 
 	 * Public --> Pode ser acessada por qualquer classe;
-	 * Private --> Pode ser acessada sÛ pela prÛpria classe;
-	 * Default/Package --> Pode ser acessada sÛ pelas classes do prÛprio pacote;
-	 * Protected --> Pode ser acessada sÛ pelas classes herdadas.
+	 * Private --> Pode ser acessada s√≥ pela pr√≥pria classe;
+	 * Default/Package --> Pode ser acessada s√≥ pelas classes do pr√≥prio pacote;
+	 * Protected --> Pode ser acessada s√≥ pelas classes herdadas.
 	 * 
 	 */
 	
 	public ClasseD1() {
 
 		ClasseP1 cp1 = new ClasseP1();
-//		ClasseD2 cd2 = new ClasseD2(); A classe Default do pacote 2 sÛ pode ser estanciada no mesmo pacote.
+//		ClasseD2 cd2 = new ClasseD2(); A classe Default do pacote 2 s√≥ pode ser estanciado no mesmo pacote.
 		ClasseP2 cp2 = new ClasseP2();
 	}
 
