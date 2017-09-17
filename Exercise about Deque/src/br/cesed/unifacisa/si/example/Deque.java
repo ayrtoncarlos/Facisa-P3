901cd8237df750f4fd69bdd1a52be35a9a059274
ayrtoncarlos <bitao.metal@gmail.com>
2017-09-17 17:01:38 -0300

    Exercise about Deque

---
+++ Exercise about Deque/src/br/cesed/unifacisa/si/example/Deque.java
@@ -0,0 +1,109 @@
+package br.cesed.unifacisa.si.example;
+
+/**
+ * 
+ * @author Ayrton Carlos
+ *
+ */
+
+public class Deque {
+
+	private static final int INITIAL_SIZE = 3;
+	private static final int INITIAL_POSITION = 0;
+	Object[] arrayDeque = new Object[INITIAL_SIZE];
+	private int inserted = 0;
+
+	public Deque() {
+
+	}
+
+	public void addFirst(Object o) {
+
+		if (o == null) {
+
+			throw new IllegalArgumentException();
+		}
+
+		else if (inserted == arrayDeque.length) {
+
+			internArrayOverflow();
+
+		} else {
+
+			for (int i = 0; i < inserted; i++) {
+
+				arrayDeque[i + 1] = arrayDeque[i];
+			}
+
+			arrayDeque[INITIAL_POSITION] = o;
+			inserted++;
+		}
+	}
+
+	public void addLast(Object o) {
+
+		if (o == null) {
+
+			throw new IllegalArgumentException();
+		}
+
+		else if (inserted == arrayDeque.length) {
+
+			internArrayOverflow();
+
+		} else {
+
+			arrayDeque[inserted] = o;
+			inserted++;
+		}
+	}
+
+	private Object[] internArrayOverflow() {
+
+		Object[] newArrayDeque = new Object[arrayDeque.length * 2];
+
+		for (int i = 0; i < arrayDeque.length; i++) {
+
+			newArrayDeque[i] = arrayDeque[i];
+		}
+
+		return arrayDeque = newArrayDeque;
+
+	}
+
+	public Object removeFirst() {
+
+		Object o = arrayDeque[INITIAL_POSITION];
+		arrayDeque[INITIAL_POSITION] = null;
+
+		for (int i = 0; i < inserted-1; i++) {
+
+			arrayDeque[i] = arrayDeque[i + 1];
+		}
+		inserted--;
+		return o;
+	}
+
+	public Object removeLast() {
+
+		Object o = arrayDeque[inserted - 1];
+		arrayDeque[inserted - 1] = null;
+		inserted--;
+		return o;
+	}
+
+	public Object getFirst() {
+
+		return arrayDeque[INITIAL_POSITION];
+	}
+
+	public Object getLast() {
+
+		return arrayDeque[inserted - 1];
+	}
+
+	public int getSize() {
+
+		return inserted;
+	}
+}
---
+++ Exercise about Deque/src/br/cesed/unifacisa/si/example/DequeTests.java
@@ -0,0 +1,123 @@
+package br.cesed.unifacisa.si.example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Ayrton Carlos
+ *
+ */
+
+public class DequeTests {
+
+	@Test
+	public void testInsertFirst1() {
+
+		Deque arrayDeque = new Deque();
+
+		arrayDeque.addFirst(3);
+		arrayDeque.addFirst(2);
+		arrayDeque.addFirst(1);
+
+		assertEquals(3, arrayDeque.getSize());
+	}
+	
+	@Test
+	public void testInsertFirst2() {
+
+		Deque arrayDeque = new Deque();
+
+		arrayDeque.addFirst('N');
+		arrayDeque.addFirst('O');
+		arrayDeque.addFirst('T');
+		arrayDeque.addFirst('R');
+		arrayDeque.addFirst('Y');
+		arrayDeque.addFirst('A');
+
+		assertEquals(5, arrayDeque.getSize());
+	}
+	
+	@Test
+	public void testRemoveFirst1() {
+
+		Deque arrayDeque = new Deque();
+
+		arrayDeque.addFirst(3);
+		arrayDeque.addFirst(2);
+		arrayDeque.addFirst(1);
+
+		assertEquals(1, arrayDeque.removeFirst());
+	}
+	
+	@Test
+	public void testRemoveFirst2() {
+
+		Deque arrayDeque = new Deque();
+
+		arrayDeque.addFirst('N');
+		arrayDeque.addFirst('O');
+		arrayDeque.addFirst('T');
+		arrayDeque.addFirst('R');
+		arrayDeque.addFirst('Y');
+		arrayDeque.addFirst('A');
+
+		assertEquals('A', arrayDeque.removeFirst());
+	}
+	
+	@Test
+	public void testInsertLast1(){
+		
+		Deque arrayDeque = new Deque();
+		
+		arrayDeque.addLast('A');
+		arrayDeque.addLast('Y');
+		arrayDeque.addLast('R');
+		arrayDeque.addLast('T');
+		arrayDeque.addLast('O');
+		arrayDeque.addLast('N');
+
+		assertEquals(5, arrayDeque.getSize());
+	}
+	
+	@Test
+	public void testInsertLast2(){
+		
+		Deque arrayDeque = new Deque();
+		
+		arrayDeque.addLast(1);
+		arrayDeque.addLast(2);
+		arrayDeque.addLast(3);
+
+		assertEquals(3, arrayDeque.getSize());
+	}
+	
+	@Test
+	public void testRemoveLast1(){
+		
+		Deque arrayDeque = new Deque();
+		
+		arrayDeque.addLast('A');
+		arrayDeque.addLast('Y');
+		arrayDeque.addLast('R');
+		arrayDeque.addLast('T');
+		arrayDeque.addLast('O');
+		arrayDeque.addLast('N');
+
+		assertEquals('N', arrayDeque.removeLast());
+	}
+	
+	@Test
+	public void testRemoveLast2(){
+		
+		Deque arrayDeque = new Deque();
+		
+		arrayDeque.addLast(1);
+		arrayDeque.addLast(2);
+		arrayDeque.addLast(3);
+
+		assertEquals(3, arrayDeque.removeLast());
+	}
+
+}
