845c2ca13a6e22c8bb574e8d4025297404b45fd8
ayrtoncarlos <31282635+ayrtoncarlos@users.noreply.github.com>
2017-09-26 00:26:20 -0300

    Update DequeFullException.java

--- P3 Challenge/src/br/cesed/unifacisa/si/example/DequeFullException.java
+++ P3 Challenge/src/br/cesed/unifacisa/si/example/DequeFullException.java
@@ -1,7 +1,16 @@
 package br.cesed.unifacisa.si.example;
 
+/**
+ * 
+ * @author Ayrton Carlos
+ *
+ */
 public class DequeFullException extends Exception {
 
+	/**
+	* constructor of class
+	*
+	*/
 	public DequeFullException(String message){
 		
 		super(message);
